2ad5269e4be0a862c2bb3513803a8231a0c29cad
Bryan Casipe <bryancasipe38@gmail.com>
2025-12-09 18:45:42 +0800

    Delete QCPLibrary directory

--- QCPLibrary/php/admin/test-session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-echo json_encode([
-    'success' => true,
-    'session' => $_SESSION,
-    'branch_id' => $_SESSION['branch_id'] ?? 'NOT SET'
-]);
